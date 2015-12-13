56f4566e028fe1d2a3366fad7428f14090db8744
Venketesh Mahapatra <mahapatra.venketesh@gmail.com>
2015-12-14 02:52:03 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors =[red,orange,green,blue,purple];
+var myName = "HOver fuvker";
+if (10<3)
+    {
+        bubbleShape = "square";
+    }
+else
+    {
+        bubbleShape = "circle";
+    }
+
+drawName(myName, letterColors);
+bounceBubbles()
+
+
+
+
+
